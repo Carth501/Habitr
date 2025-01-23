65e4668e1799faf5dff3e3d6214ac7f4e85cb378
carth501 <carth501@gmail.com>
2025-01-22 19:56:22 -0500

    Add sum function and Vitest configuration with initial test

--- apps/habitr-client/vite.config.ts
+++ apps/habitr-client/vite.config.ts
@@ -1,14 +1,14 @@
-import react from '@vitejs/plugin-react'
-import path from "path"
-import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [react()],
-  envDir: './environments',
-  server: {port: 4001},
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-})
+	plugins: [react()],
+	envDir: './environments',
+	server: { port: 4001 },
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+		},
+	},
+});
